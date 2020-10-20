5d7d091f1d0bf53b6de4f179dd1f48bba06e7479
VKotrunov <57618856+VKotrunov@users.noreply.github.com>
2020-10-20 11:00:00 +0200

    Agregar archivos de proyecto.

---
+++ UT2_Ac1_AdivinarNumero/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace UT2_Ac1_AdivinarNumero
+{
+    
+    public partial class MainWindow : Window
+    {
+        private int numeroGenerado;
+        public MainWindow()
+        {
+            InitializeComponent();
+            numeroGenerado = GeneraNumeroAleatorio();
+        }
+        private int GeneraNumeroAleatorio() 
+        {
+            Random seed = new Random();
+            return seed.Next(0,101);
+        }
+        private void comprobarButton_Click(object sender, RoutedEventArgs e)
+        {
+            int numeroUsuario = int.Parse(inputTextBox.Text);
+            if (numeroGenerado == numeroUsuario)
+            {
+                resultadoTextBlock.Text = "¡Correcto!";
+            }
+            else if (numeroGenerado > numeroUsuario)
+            {
+                resultadoTextBlock.Text = "¡El número es mayor!";
+            }
+            else 
+            {
+                resultadoTextBlock.Text = "¡El número es menor!";
+            }
+        }
+
+        private void reiniciarButton_Click(object sender, RoutedEventArgs e)
+        {
+            numeroGenerado = GeneraNumeroAleatorio();
+            inputTextBox.Text = "";
+            resultadoTextBlock.Text = "";
+        }
+    }
+}
